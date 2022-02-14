59db78feaadbd167f86408f7706b2c34f5fe216a
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-02-14 21:40:11 +0800

    Fix compile

--- src/dialogue_box.cpp
+++ src/dialogue_box.cpp
@@ -1,10 +1,12 @@
 #include "dialogue_box.h"
 
+#include <GodotGlobal.hpp>
+
 using namespace godot;
 
 void DialogueBox::_register_methods() {
-
 }
 
 void DialogueBox::_init() {
+    Godot::print("rua");
 }
